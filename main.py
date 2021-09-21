010e503df774859ceed789b919ab386a02164945
statisdisc <will.a.m@hotmail.co.uk>
2021-09-21 20:26:10 +0100

    Added rolling mean calculation and plots

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import numpy as np
 
 # User-made modules
 from src.objects.pathSetup import pathSetup
+from src.plots.plotRollingMean import plotRollingMean
 from src.utilities import download
 from src.utilities.timeElapsed import timeElapsed
+
+def addRollingMean(data, column, years=1):
+    # Note, you cannot use f"{years}y" or f"{12*years}m" because a month/year can vary in length
+    # E.g. 28 days vs 31 days in a month, 365 days vs 366 days in a year 
+    period = f"{int(365.2425*years)}d"
+    data[f"{column}-rolling-{years}"] = data.rolling(period, min_periods=1)[column].mean()
+    return data
+
+
 
 @timeElapsed
 def main():
@@ -12,9 +22,16 @@ def main():
     
     years = np.arange(1981,2021,1)
     data = download.downloadOrLoad(years, folder.data)
-    print(data.head())
+    
+    data = addRollingMean(data, "Temperature", years=10)
+    data = addRollingMean(data, "Temperature", years=30)
+    
+    plotRollingMean(data, folder.outputs)
+    
+    print(data.head(10))
+    print(data.tail(10))
     print(data.info())
-    print(data.describe())
+    # print(data.describe())
 
 if __name__ == "__main__":
     main()
---
+++ src/plots/plotRollingMean.py
@@ -0,0 +1,22 @@
+import os
+import matplotlib.pyplot as plt
+
+def plotRollingMean(data, folder):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,10))
+    
+    data = data.loc['1991-01-01':'1992-01-01']
+    
+    # data["Temperature"]           .plot(ax=ax, linewidth=0.5, color="#999999")
+    data["Temperature-rolling-10"].plot(ax=ax, linewidth=1.0, color="blue")
+    data["Temperature-rolling-30"].plot(ax=ax, linewidth=1.0, color="orange")
+    
+    ax.set_xlabel("Date")
+    ax.set_ylabel(r"Temperature ($\deg$F)")
+    ax.legend(loc="best")
+    
+    plt.savefig(
+        os.path.join(folder, "rolling-mean.png"),
+        bbox_inches = "tight",
+        dpi = 300
+    )
+    plt.close()
